671d2ac9239f65533b64a1cb4bb4c3a23db42431
Rizz <flshnglights@gmail.com>
2019-03-12 02:02:33 +0300

    Task 2 - Methods, MaxValue The work is done.

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,30 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        taskMain(-15, 15);
+    }
+
+    private static void taskMain(int from, int to){
         Random rand = new Random();
 
-        int[] arr = {-1, 0, 1, 1, 1};
-        //int[] arr = new int[5];
-        int[] stat = new int[5];
+        int[] arr = new int[20];
+        int[] stat = new int[20];
         int i;
-        int j;
-        int cnt;
 
-        /*for(i = 0; i < 5; i++){
-            arr[i] = (rand.nextInt(3) - 1);
-        }*/
+        System.out.print("Массив: ");
+        for(i = 0; i < 20; i++){
+            arr[i] = (rand.nextInt((to - from) + 1) + from);
+            System.out.printf("%d ", arr[i]);
+        }
+        elemCalc(arr, stat);
+        maxValue(arr, stat);
+    }
 
-        for(i = 0; i < 5; i++){
-            cnt = 1;
-            for(j = i + 1; j < 5; j++){
+    private static void elemCalc(int[] arr, int[] stat){
+        int i, j;
+        for(i = 0; i < 20; i++){
+            int cnt = 1;
+            for(j = i + 1; j < 20; j++){
                 if(arr[i] == arr[j]){
                     cnt++;
                     stat[j] = -1;
@@ -28,27 +36,33 @@ public class Main {
                 stat[i] = cnt;
             }
         }
-        System.out.println("Array");
-        for(i = 0; i < 5; i++){
+        System.out.println("\n/----------------------------------------/");
+        for(i = 0; i < 20; i++){
             if(stat[i]!= -1){
-                System.out.println("Число " + arr[i] + " выводится " + stat[i] + " раз(а)");
+                System.out.println("Число '" + arr[i] + "' встречается " + stat[i] + " раз(а)");
             }
-
         }
+        System.out.println("/----------------------------------------/");
+    }
 
-        for(i=0;i<5;i++){
-            System.out.println(arr[i]+"       "+stat[i]);
-        }
-
+    private static void maxValue(int[] arr, int[] stat){
+        int i;
         int maxIndex = 0;
-        for(i=1; i<5; i++){
-            if(stat[i] > stat[maxIndex]){
+        for(i = 1; i < 20; i++) {
+            if (stat[i] > stat[maxIndex]) {
                 maxIndex = i;
-                System.out.println("Чаще всего встречается: " + arr[maxIndex] + " ---> " + stat[maxIndex] + " раз(а)");
+                System.out.println("Чаще всего встречается '" + arr[maxIndex] + "' ---> " + stat[maxIndex] + " раз(а)");
             }
         }
-
-        System.out.println(maxIndex);
-
+        if(maxIndex == 0){
+            System.out.println("Чаще всего встречается '" + arr[0] + "' ---> " + stat[0] + " раз(а)");
+        }
+        for(i = 0; i < 20; i++){
+            if((stat[maxIndex] == stat [i]) && (maxIndex != i)){
+                System.out.println("Чаще всего встречается '" + arr[i] + "' ---> " + stat[i] + " раз(а)");
+            }
+        }
     }
 }
+
+
